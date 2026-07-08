fbddd97f34f59fd21d969697536dd1b481091590
Eric <96599563+RickNE7@users.noreply.github.com>
2026-07-08 14:38:06 -0500

    Add files via upload

---
+++ Planificación en Negocios Internacionales 26-2-564P-A/EA2/assets/chunk-FMBD7UC4-D_LVL5sF.js
@@ -0,0 +1,15 @@
+import{_ as e}from"./InformePage-BH7_IKFW.js";var l=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as g};
